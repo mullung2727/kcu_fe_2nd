93d821fbf85f22736108cac6f60168a4e8b3f9b7
정원준 <mullung27@gmail.com>
2026-03-20 07:34:55 +0900

    260320 start

---
+++ src/react_study/ex02_useState/ex07_filter.jsx
@@ -0,0 +1,16 @@
+export default function Filter() {
+  const products = [
+    { name: "노트북", price: 1200000, description: "고성능 업무용 노트북" },
+    { name: "키보드", price: 35000, description: "기계식 키보드" },
+    { name: "마우스", price: 9000, description: "무선 마우스" },
+    { name: "이어폰", price: 9000, description: "가성비 좋은 유선 이어폰" },
+    { name: "모니터", price: 210000, description: "27인치 FHD 모니터" },
+  ];
+
+  return (
+    <div>
+
+    </div>
+  )
+
+}
---
+++ src/react_study/ex02_useState/ex08_prev.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+
+const cardStyle = {
+  backgroundColor: "#4C4E58",
+  color: "#fff",
+  padding: "40px",
+  
+  borderRadius: "12px",
+  width: "50%",
+  margin: "50px auto",
+  display: "flex",
+  flexDirection: "column",
+  gap: "30px",
+};
+
+const fieldStyle = { 
+  background: "#72757C", 
+  border: "none",
+  borderRadius: "10px",
+  padding: "8px 4px", 
+  color: "#FDFDFD", 
+  fontSize: "25px" 
+};
+
+const wrapperStyle = {
+  width: "50%",
+  margin: "40px auto",
+  backgroundColor: "#383B44",
+  padding: "20px",
+  borderRadius: "12px"
+};
+
+const cardNameStyle = {
+  fontWeight: "bold",
+  color: "#FDFDFD",
+  fontSize: "25px"
+};
+
+const deleteButtonStyle = {
+  backgroundColor: "red",
+  border: "none",
+  borderRadius: "10px",
+  color: "#fff",
+  padding: "4px 8px"
+};
+
+const fieldGroupStyle = {
+  display: "flex",
+  flexDirection: "column",
+  gap: "8px"
+};
+
+const labelStyle = {
+  fontSize: "14px",
+  color: "#FDFDFD"
+};
+
+const cardDescStyle = {
+  color: "#FDFDFD",
+  textAlign: "left"
+};
+
+
+export default function AddRemove() {
+
+  const changeHandler = (e) => {
+
+  };
+
+  const handleSubmit = (e) => {
+
+  }
+
+  return (
+
+    <div style={wrapperStyle}>
+      
+    </div>
+  )
+}
